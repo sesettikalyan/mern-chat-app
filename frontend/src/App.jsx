e9af19ef3c5fb1e8bd1b8b625a162deec4860ccb
manoharlikhit <gouthamvenkat2002@gmail.com>
2024-12-07 23:54:39 +0530

    Add map component with geolocation and range filtering functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,36 @@ import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
+import MapComponent from "./pages/home/map";
 
 function App() {
-	const { authUser } = useAuthContext();
-	return (
-		<div className='p-4 h-screen flex items-center justify-center'>
-			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
-				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
-			</Routes>
-			<Toaster />
-		</div>
-	);
+  const { authUser } = useAuthContext();
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? (
+              //  <Home />
+              <MapComponent />
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
